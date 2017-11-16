567fab74a9f83c84b9b83ef6c252feadffa45995
Caitlin Barron <barroncn@gmail.com>
2017-11-16 17:29:05 +0000

    Outline

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAAi7s87SW5XOtn5dKwIBwEIZsY4tBZfkg",
+    authDomain: "ryde-or-dryve.firebaseapp.com",
+    databaseURL: "https://ryde-or-dryve.firebaseio.com",
+    projectId: "ryde-or-dryve",
+    storageBucket: "ryde-or-dryve.appspot.com",
+    messagingSenderId: "998231386924"
+  };
+  firebase.initializeApp(config);
