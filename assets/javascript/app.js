420bd75fd574f02e407bda7599d6e2185b0eab82
Caitlin Barron <barroncn@gmail.com>
2017-11-20 19:01:32 +0000

    Rough MVP

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,220 +13,179 @@ var startLocation = "";
 var destination = "";
 var mpg = "";
 
-var samplePriceResultsUber = {
-    "prices": [
-        {
-            "localized_display_name": "SELECT",
-            "distance": 6.25,
-            "display_name": "SELECT",
-            "product_id": "57c0ff4e-1493-4ef9-a4df-6b961525cf92",
-            "high_estimate": 39,
-            "low_estimate": 31,
-            "duration": 1140,
-            "estimate": "$31-39",
-            "currency_code": "USD"
-        },
-        {
-            "localized_display_name": "uberXL",
-            "distance": 6.25,
-            "display_name": "uberXL",
-            "product_id": "821415d8-3bd5-4e27-9604-194e4359a449",
-            "high_estimate": 27,
-            "low_estimate": 21,
-            "duration": 1140,
-            "estimate": "$21-27",
-            "currency_code": "USD"
-        },
-        {
-            "localized_display_name": "BLACK",
-            "distance": 6.25,
-            "display_name": "BLACK",
-            "product_id": "d4abaae7-f4d6-4152-91cc-77523e8165a4",
-            "high_estimate": 50,
-            "low_estimate": 40,
-            "duration": 1140,
-            "estimate": "$40-50",
-            "currency_code": "USD"
-        },
-        {
-            "localized_display_name": "SUV",
-            "distance": 6.25,
-            "display_name": "SUV",
-            "product_id": "8920cb5e-51a4-4fa4-acdf-dd86c5e18ae0",
-            "high_estimate": 63,
-            "low_estimate": 51,
-            "duration": 1140,
-            "estimate": "$51-63",
-            "currency_code": "USD"
-        },
-        {
-            "localized_display_name": "ASSIST",
-            "distance": 6.25,
-            "display_name": "ASSIST",
-            "product_id": "ff5ed8fe-6585-4803-be13-3ca541235de3",
-            "high_estimate": 21,
-            "low_estimate": 16,
-            "duration": 1140,
-            "estimate": "$16-21",
-            "currency_code": "USD"
-        },
-        {
-            "localized_display_name": "WAV",
-            "distance": 6.25,
-            "display_name": "WAV",
-            "product_id": "2832a1f5-cfc0-48bb-ab76-7ea7a62060e7",
-            "high_estimate": 21,
-            "low_estimate": 16,
-            "duration": 1140,
-            "estimate": "$16-21",
-            "currency_code": "USD"
-        },
-        {
-            "localized_display_name": "POOL",
-            "distance": 6.25,
-            "display_name": "POOL",
-            "product_id": "26546650-e557-4a7b-86e7-6a3942445247",
-            "high_estimate": 15,
-            "low_estimate": 11,
-            "duration": 1140,
-            "estimate": "$11-14",
-            "currency_code": "USD"
-        },
-        {
-            "localized_display_name": "uberX",
-            "distance": 6.25,
-            "display_name": "uberX",
-            "product_id": "a1111c8c-c720-46c3-8534-2fcdd730040d",
-            "high_estimate": 21,
-            "low_estimate": 16,
-            "duration": 1140,
-            "estimate": "$16-21",
-            "currency_code": "USD"
-        },
-        {
-            "localized_display_name": "TAXI",
-            "distance": 6.25,
-            "display_name": "TAXI",
-            "product_id": "3ab64887-4842-4c8e-9780-ccecd3a0391d",
-            "high_estimate": null,
-            "low_estimate": null,
-            "duration": 1140,
-            "estimate": "Metered",
-            "currency_code": null
-        }
-    ]
-};
-
-
-var sampleTimeResultsUber = {
-  "times": [
-        {
-            "localized_display_name": "SELECT",
-            "estimate": 180,
-            "display_name": "SELECT",
-            "product_id": "57c0ff4e-1493-4ef9-a4df-6b961525cf92"
-        },
-        {
-            "localized_display_name": "uberXL",
-            "estimate": 480,
-            "display_name": "uberXL",
-            "product_id": "821415d8-3bd5-4e27-9604-194e4359a449"
-        },
-        {
-            "localized_display_name": "BLACK",
-            "estimate": 240,
-            "display_name": "BLACK",
-            "product_id": "d4abaae7-f4d6-4152-91cc-77523e8165a4"
-        },
-        {
-            "localized_display_name": "SUV",
-            "estimate": 240,
-            "display_name": "SUV",
-            "product_id": "8920cb5e-51a4-4fa4-acdf-dd86c5e18ae0"
-        },
-        {
-            "localized_display_name": "ASSIST",
-            "estimate": 540,
-            "display_name": "ASSIST",
-            "product_id": "ff5ed8fe-6585-4803-be13-3ca541235de3"
-        },
-        {
-            "localized_display_name": "WAV",
-            "estimate": 300,
-            "display_name": "WAV",
-            "product_id": "2832a1f5-cfc0-48bb-ab76-7ea7a62060e7"
-        },
-        {
-            "localized_display_name": "POOL",
-            "estimate": 60,
-            "display_name": "POOL",
-            "product_id": "26546650-e557-4a7b-86e7-6a3942445247"
-        },
-        {
-            "localized_display_name": "uberX",
-            "estimate": 60,
-            "display_name": "uberX",
-            "product_id": "a1111c8c-c720-46c3-8534-2fcdd730040d"
-        },
-        {
-            "localized_display_name": "TAXI",
-            "estimate": 480,
-            "display_name": "TAXI",
-            "product_id": "3ab64887-4842-4c8e-9780-ccecd3a0391d"
-        }
-    ]
-}
  
-  $("#submit").on("click", function(event){
-    event.preventDefault();
-    startLocation = $("#startLocation").val();
-    destination = $("#destination").val();
-    mpg = $("#mpg").val();
+$("#submit").on("click", function(event){
     
-    if((startLocation !== "") && (destination !== "") && (mpg !== "")){
+    var startLat = "37.7752315"; // This will be the starting latitude we get from the Google API (from the users starting address input)
+    var startLong = "-122.418075"; // This will be the starting longitude...
+    var endLat = "37.7752415"; // This will be the destination lattitude we get from the Google API (from the users destination input)
+    var endLong = "-122.518075"; // This wil be the destination longitude...
+      
+    event.preventDefault();
+    startLocation = $("#startLocation").val(); //Do we need to .trim() this? 
+    destination = $("#destination").val(); // Do we need to .trim() this?
+    mpg = $("#mpg").val(); // Same... .trim()?
+    
+    //if((startLocation !== "") && (destination !== "") && (mpg !== "")){
+        
+        // I'm thinking maybe we don't want to empty out the inputs, in case the user wants to go back and change one thing (I know I'm the one who put them in, just rethinking it)
         $("#startLocation").val("");
         $("#destination").val("");
         $("#mpg").val("");
         $("#message").html("");
-      }
+        
+        uberInfo(startLat, startLong, endLat, endLong); //This will call the function to get the Uber Info, using the coordinate variables created above
+        lyftInfo(startLat, startLong, endLat, endLong); //Same for the lyft info function
+     // }
       
-    else{
-      $("#message").html("Please input all information");
-    }
+    // else{
+    //   $("#message").html("Please input all information");
+    // }
+});
+
+
+//UBER
+//============================================================================================================
+
+function uberInfo(y, x , yy, xx){   //starting latitude=x, starting longitude = y, ending latitude = xx, ending longitude = yy
+   
+    //This token will need to be updated
+    var token = "KA.eyJ2ZXJzaW9uIjoyLCJpZCI6InlaL3ZIdkJnU05TUkZFeTdiUFZuQVE9PSIsImV4cGlyZXNfYXQiOjE1MTM1NjU1NTQsInBpcGVsaW5lX2tleV9pZCI6Ik1RPT0iLCJwaXBlbGluZV9pZCI6MX0.OiMPjfswSr6QI5IdKzHDro8udDWi4Ok0C5a1oKrO_kQ";
     
-  });
-  
-//   var settings = {
-//   "async": true,
-//   "crossDomain": true,
-//   "url": "https://api.uber.com/v1.2/estimates/price?latitude=37.7759792&longitude=-122.41823",
-//   "method": "GET",
-//   "headers": {
-//     "authorization": "Bearer xmwkq2JJhPFoG8stW27xD4KHcKh62c_iRIKZJ19v",
-//     "cache-control": "no-cache",
-// //     "postman-token": "26f81f12-4a8e-46af-6e27-0ba915f1d4f1"
-// }
-// };
-
-// $.ajax(settings).done(function (response) {
-//   console.log(response);
-// });
-
-//This is the code Jeff is going over for the CORS issues
-$.ajax({
-          url: "https://api.uber.com/v1.2/estimates/time", 
-          beforeSend: function(xhr) { 
-            xhr.setRequestHeader("Authorization", "Token" + btoa("KA.eyJ2ZXJzaW9uIjoyLCJpZCI6InNLVFJjOGlFVFdTSFNEc24rZm5YOHc9PSIsImV4cGlyZXNfYXQiOjE1MTM1MjA5NzQsInBpcGVsaW5lX2tleV9pZCI6Ik1RPT0iLCJwaXBlbGluZV9pZCI6MX0.5Yumx3RvUwqXiRzabQnWplP1C-gl9K2ZU9WRlB2Ws4E")); 
-          },
-          type: 'GET',
-          dataType: 'json',
-          contentType: 'application/json',
-          processData: false,
-          data: "start_latitude=37.7752315&start_longitude=-122.418075",
-          success: function (data) {
-            alert(JSON.stringify(data));
-          },
-          error: function(){
-            alert("Cannot get data");
-          }
+    // AJAX request for Uber's time estimate information    
+    $.ajax({
+            url: "https://api.uber.com/v1.2/estimates/time",
+            method: "GET",
+            data: {
+                start_latitude: y,
+                start_longitude: x,
+                access_token: token
+            },
+            success: function(uberTimeResults) {
+                console.log(uberTimeResults);
+                //This loop will ensure we get the data for uberX, we can easily add another if statement to set a variable in case we want to an estimate for other types of uber
+                for(var i = 0; i< uberTimeResults.times.length; i++){
+                    if("uberX" === uberTimeResults.times[i].display_name){
+                         var uberXindex = i;
+                    }
+                }
+                var minutesTilUber = Math.round(uberTimeResults.times[uberXindex].estimate / 60); //Minutes away of the closest Uber
+                 console.log("minutes til next uber: " + minutesTilUber);
+            }
         });
+
+    // AJAX request for Uber's price estimate information. This will also give us the distance and duration of the trip.
+    $.ajax({
+        url: "https://api.uber.com/v1.2/estimates/price",
+            method: "GET",
+            data: {
+                start_latitude: y,
+                start_longitude: x,
+                end_latitude: yy,
+                end_longitude: xx,
+                access_token: token
+            },
+            success: function (uberPriceResults) {
+                console.log(uberPriceResults);
+                for(var i = 0; i< uberPriceResults.prices.length; i++){
+                     if("uberX" === uberPriceResults.prices[i].display_name){
+                         var uberXindex = i;
+                     }
+                }
+                var uberXprice = uberPriceResults.prices[uberXindex].estimate;  //The range of the Uber Price Estimate
+                var uberXdistance = uberPriceResults.prices[uberXindex].distance;    // The distance of the trip in miles
+                var uberXduration = Math.round(uberPriceResults.prices[uberXindex].duration / 60);    //The minutes the trip will take
+                var uberAverageXprice = Math.round((uberPriceResults.prices[uberXindex].high_estimate + uberPriceResults.prices[uberXindex].low_estimate) / 2); //The average of the range of prices in case we want Jim to get his way
+                 console.log("price range for uber: " + uberXprice);
+                 console.log("Distance of the trip: " + uberXdistance + " miles");
+                 console.log("Time the trip takes: " + uberXduration + " minutes");
+                 console.log("The average of the price range estimate for uber: " + uberAverageXprice);
+            }
+    });
+}
+
+
+
+//LYFT
+//=============================================================================================================
+ 
+function lyftInfo(y, x, yy, xx){
+    
+    var lyftToken;
+    var clientId = '3TY5gGnnFF3h';
+    var clientSecret = 'BoWG3zPcjYe0HSqOvG4IKuIjFm5dT9hM';
+    
+    //Requests a token
+    $.ajax({
+      url: 'https://api.lyft.com/oauth/token',
+      type: 'POST',
+      data: {
+        grant_type: 'client_credentials',
+        scope: 'public'
+      },
+      beforeSend: function (xhr) {
+        xhr.setRequestHeader ("Authorization", "Basic " + btoa(clientId + ":" + clientSecret));
+      },
+      success: function(response) {
+        console.log(response);
+        lyftToken = response.access_token;
+        console.log(lyftToken)
+      
+    //Uses token to request driver ETA information
+        var lyftETAData = {
+          "async": true,
+          "crossDomain": true,
+          "url": "https://api.lyft.com/v1/eta?lat=" + y + "&lng=" + x,
+          "method": "GET",
+          "headers": {
+            "authorization": "Bearer " + lyftToken
+          }
+        };
+        
+        $.ajax(lyftETAData).done(function (lyftTimeResults) {
+                        console.log(lyftTimeResults);
+                        for(var i = 0; i< lyftTimeResults.eta_estimates.length; i++){
+                             if("Lyft" === lyftTimeResults.eta_estimates[i].display_name){
+                                 var lyftindex = i;
+                             }
+                        }
+                        var minutesTilLyft = Math.round(lyftTimeResults.eta_estimates[lyftindex].eta_seconds / 60);
+                        console.log("minutes til next lyft: " + minutesTilLyft);
+        });
+        
+    //Uses token to request price estimate
+        var lyftPriceData = {
+          "async": true,
+          "crossDomain": true,
+          "url": "https://api.lyft.com/v1/cost?start_lat=" + y + "&start_lng=" + x + "&end_lat=" + yy + "&end_lng=" + xx,
+          "method": "GET",
+          "headers": {
+            "authorization": "Bearer " + lyftToken
+          }
+        };
+    
+        $.ajax(lyftPriceData).done(function (lyftPriceResults) {
+                        console.log(lyftPriceResults);
+                        for(var i = 0; i< lyftPriceResults.cost_estimates.length; i++){
+                             if("Lyft" === lyftPriceResults.cost_estimates[i].display_name){
+                                 var lyftindex = i;
+                             }
+                        }
+                        var lyftAveragePrice = Math.round((lyftPriceResults.cost_estimates[lyftindex].estimated_cost_cents_min + lyftPriceResults.cost_estimates[lyftindex].estimated_cost_cents_max) / 200);
+                        console.log("lyft average price: " + lyftAveragePrice);
+                        if (lyftPriceResults.cost_estimates[lyftindex].estimated_cost_cents_min !== lyftPriceResults.cost_estimates[lyftindex].estimated_cost_cents_max){
+                            var lyftPrice = "$" + Math.round(lyftPriceResults.cost_estimates[lyftindex].estimated_cost_cents_min / 100) + "-" + Math.round(lyftPriceResults.cost_estimates[lyftindex].estimated_cost_cents_max / 100);
+                        }
+                        else{
+                            var lyftPrice = "$" + Math.round(lyftPriceResults.cost_estimates[lyftindex].estimated_cost_cents_min / 100);
+                        }
+                        console.log("Price estimate for lyft: " + lyftPrice);
+        });
+      
+      
+      } //ends success
+
+    }); //ends initial AJAX request
+  
+} //ends function
+
--- assets/javascript/snippets.js
+++ assets/javascript/snippets.js
@@ -1,17 +1,17 @@
-//****here is some code that should work for determining geolocation assuming
+// ****here is some code that should work for determining geolocation assuming
 // the user browswer supports it.  I think we shoudl have a check-box for the
 // user to give our app permision to use their location - Jim
-//-----------------------------------------------------
-// function getLocation() {
-//     if (navigator.geolocation) {
-//         navigator.geolocation.getCurrentPosition(showPosition);
-//     } else { 
-//         x.innerHTML = "Geolocation is not supported by this browser.";
-//     }
-// }
+// -----------------------------------------------------
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
 
-// function showPosition(position) {
-//     x.innerHTML = "Latitude: " + position.coords.latitude + 
-//     "<br>Longitude: " + position.coords.longitude;
-// }
-//-----------------------------------------------------
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude;
+}
+// -----------------------------------------------------
